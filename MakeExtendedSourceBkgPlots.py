0ecb415c35b58c6d32ba2d8fb51cc45659ed99bc
ruoyushang <r.y.shang@gmail.com>
2018-10-29 12:30:07 -0700

    update

--- MakeExtendedSourceBkgPlots.py
+++ MakeExtendedSourceBkgPlots.py
@@ -3,29 +3,39 @@ import sys,ROOT
 
 ROOT.gStyle.SetOptStat(0)
 ROOT.TH1.SetDefaultSumw2()
+ROOT.TH1.AddDirectory(False)
 ROOT.gStyle.SetPaintTextFormat("0.3f")
 
-source  = 'IC443'
-#source  = 'PKS1424'
-#method = 'EmissionHeight'
-method = 'MSCW'
-
-FilePath = 'output/Histograms_%s_%s.root'%(source,method)
-print 'Read %s'%(FilePath)
-InputFile=ROOT.TFile(FilePath)
-
-Elev_lower_cut = 70
-Elev_upper_cut = 80
-Azim_lower_cut = 40
-Azim_upper_cut = 60
-Erec_lower_cut = 500
-Erec_upper_cut = 1000
-MSCL_lower_cut = -2
-MSCL_upper_cut = 0.7
+folder = 'output_Oct28'
 MSCW_lower_cut = -2
 MSCW_upper_cut = 0.7
+#folder = 'output_Oct29'
+#MSCW_lower_cut = 2
+#MSCW_upper_cut = 3
 
-Variable = '_theta2_'
+source  = '2ndCrab'
+#source  = 'PKS1424'
+#source  = '3C264'
+#source  = 'H1426'
+#source  = 'IC443'
+#source  = 'Ton599'
+
+Elev_lower_cut = 60
+Elev_upper_cut = 90
+Azim_lower_cut = 0
+Azim_upper_cut = 80
+Erec_lower_cut = 100
+Erec_upper_cut = 10000
+MSCL_lower_cut = -2
+MSCL_upper_cut = 0.7
+
+#Variable = '_Energy_'
+#xtitle = 'E [TeV]'
+#Variable = '_theta2_'
+#xtitle = '#theta^{2}'
+Variable = '_theta2zoomin_'
+xtitle = '#theta^{2}'
+#Variable = '_XoffVsYoff_'
 
 def set_histStyle( hist , color):
     hist.SetFillColor(color)
@@ -145,7 +155,7 @@ def MakeStackPlot(Hist_Data,Hist_CR,title,name):
         Hist_Ratio.SetBinError(b+1,0)
     Hist_Ratio.SetFillColor(1)
     Hist_Ratio.Draw("B same")
-    if title=='Erec':
+    if title=='E [TeV]':
         pad1.SetLogy()
         pad1.SetLogx()
         pad2.SetLogx()
@@ -155,55 +165,139 @@ def MakeStackPlot(Hist_Data,Hist_CR,title,name):
     pad2.SetLogx(0)
 
 
-HistList = InputFile.GetListOfKeys()
+def SelectHistograms(method,isData):
+
+    Hist_Data = ROOT.TH1D("Hist_Data","",1,0,1)
+
+    FilePath = '%s/Histograms_%s_%s.root'%(folder,source,method)
+    print 'Read %s'%(FilePath)
+    InputFile=ROOT.TFile(FilePath)
+    
+    HistList = InputFile.GetListOfKeys()
+    
+    print 'HistList.GetSize() = %s'%(HistList.GetSize())
+    for h in range(0,HistList.GetSize()):
+        name = HistList.At(h).GetName()
+        hist = InputFile.Get(name)
+        Elev_lower = 0
+        Elev_upper = 0
+        Azim_lower = 0
+        Azim_upper = 0
+        Erec_lower = 0
+        Erec_upper = 0
+        if not Variable in name: continue
+        for e in range(0,len(name.split("_"))):
+            element = name.split("_")[e]
+            if 'Elev' in element:
+                element = element.strip('Elev')
+                Elev_lower = float(element.split('to')[0])
+                Elev_upper = float(element.split('to')[1])
+            if 'Azim' in element:
+                element = element.strip('Azim')
+                Azim_lower = float(element.split('to')[0])
+                Azim_upper = float(element.split('to')[1])
+            if 'Erec' in element and 'to' in element:
+                element = element.strip('Erec')
+                Erec_lower = float(element.split('to')[0])
+                Erec_upper = float(element.split('to')[1])
+        if Elev_lower<Elev_lower_cut: continue
+        if Elev_upper>Elev_upper_cut: continue
+        if Azim_lower<Azim_lower_cut: continue
+        if Azim_upper>Azim_upper_cut: continue
+        if Erec_lower<Erec_lower_cut: continue
+        if Erec_upper>Erec_upper_cut: continue
+        if isData in name: 
+            if Hist_Data.Integral()==0:
+                Hist_Data.Delete()
+                Hist_Data = hist.Clone()
+            else:
+                Hist_Data.Add(hist)
+
+    return Hist_Data
+
+Hist_Data1 = SelectHistograms('Height','Data')
+Hist_CR1 = SelectHistograms('Height','CR')
+Hist_Data2 = SelectHistograms('MSCW','Data')
+Hist_CR2 = SelectHistograms('MSCW','CR')
+
+print 'Hist_Data1.Integral() = %s'%(Hist_Data1.Integral())
+print 'Hist_Data2.Integral() = %s'%(Hist_Data2.Integral())
 
 tag = Variable.strip('_')
+tag += '_%s'%(source)
+tag += '_%s'%('EmissionHeight')
+if MSCW_lower_cut>1:
+    tag += '_Validation'
+MakeStackPlot(Hist_Data1,Hist_CR1,xtitle,tag)
 
-print 'HistList.GetSize() = %s'%(HistList.GetSize())
-Hist_Data = ROOT.TH1D("Hist_Data","",1,0,1)
-Hist_CR = ROOT.TH1D("Hist_CR","",1,0,1)
-for h in range(0,HistList.GetSize()):
-    name = HistList.At(h).GetName()
-    hist = InputFile.Get(name)
-    Elev_lower = 0
-    Elev_upper = 0
-    Azim_lower = 0
-    Azim_upper = 0
-    Erec_lower = 0
-    Erec_upper = 0
-    if not Variable in name: continue
-    for e in range(0,len(name.split("_"))):
-        element = name.split("_")[e]
-        if 'Elev' in element:
-            element = element.strip('Elev')
-            Elev_lower = float(element.split('to')[0])
-            Elev_upper = float(element.split('to')[1])
-        if 'Azim' in element:
-            element = element.strip('Azim')
-            Azim_lower = float(element.split('to')[0])
-            Azim_upper = float(element.split('to')[1])
-        if 'Erec' in element and 'to' in element:
-            element = element.strip('Erec')
-            Erec_lower = float(element.split('to')[0])
-            Erec_upper = float(element.split('to')[1])
-    if Elev_lower<Elev_lower_cut: continue
-    if Elev_upper>Elev_upper_cut: continue
-    if Azim_lower<Azim_lower_cut: continue
-    if Azim_upper>Azim_upper_cut: continue
-    if Erec_lower<Erec_lower_cut: continue
-    if Erec_upper>Erec_upper_cut: continue
-    if 'Data' in name: 
-        if Hist_Data.Integral()==0:
-            Hist_Data.Delete()
-            Hist_Data = hist.Clone()
-        else:
-            Hist_Data.Add(hist)
-    else: 
-        if Hist_CR.Integral()==0:
-            Hist_CR.Delete()
-            Hist_CR = hist.Clone()
-        else:
-            Hist_CR.Add(hist)
+tag = Variable.strip('_')
+tag += '_%s'%(source)
+tag += '_%s'%('MSCW')
+if MSCW_lower_cut>1:
+    tag += '_Validation'
+MakeStackPlot(Hist_Data2,Hist_CR2,xtitle,tag)
 
-MakeStackPlot(Hist_Data,Hist_CR,'E [TeV]',tag)
+Hist_CR3 = Hist_CR1.Clone()
+for b in range(0,Hist_CR3.GetNbinsX()):
+    CR1 = Hist_CR1.GetBinContent(b+1)
+    ErrCR1 = Hist_CR1.GetBinError(b+1)
+    CR2 = Hist_CR2.GetBinContent(b+1)
+    ErrCR2 = Hist_CR2.GetBinError(b+1)
+    CR3 = 0
+    ErrCR3 = 0
+    if not ErrCR1==0 and not ErrCR2==0:
+        CR3 = (CR1/(ErrCR1*ErrCR1) + CR2/(ErrCR2*ErrCR2))/(1./(ErrCR1*ErrCR1) + 1./(ErrCR2*ErrCR2))
+        ErrCR3 = 1./pow((pow(1./ErrCR1,2) + pow(1./ErrCR2,2)),0.5)
+        Hist_CR3.SetBinContent(b+1,CR3)
+        Hist_CR3.SetBinError(b+1,ErrCR3)
+tag = Variable.strip('_')
+tag += '_%s'%(source)
+tag += '_%s'%('Combined')
+if MSCW_lower_cut>1:
+    tag += '_Validation'
+MakeStackPlot(Hist_Data1,Hist_CR3,xtitle,tag)
 
+
+Hist_Sensitivity1 = Hist_Data1.Clone()
+Hist_Sensitivity2 = Hist_Data1.Clone()
+Hist_Sensitivity3 = Hist_Data1.Clone()
+Hist_Sensitivity1.Reset()
+Hist_Sensitivity2.Reset()
+Hist_Sensitivity3.Reset()
+for b in range(0,Hist_CR3.GetNbinsX()):
+    CR1 = Hist_CR1.GetBinContent(b+1)
+    ErrCR1 = Hist_CR1.GetBinError(b+1)
+    CR2 = Hist_CR2.GetBinContent(b+1)
+    ErrCR2 = Hist_CR2.GetBinError(b+1)
+    CR3 = 0
+    ErrCR3 = 0
+    if not ErrCR1==0 and not ErrCR2==0:
+        CR3 = (CR1/(ErrCR1*ErrCR1) + CR2/(ErrCR2*ErrCR2))/(1./(ErrCR1*ErrCR1) + 1./(ErrCR2*ErrCR2))
+        ErrCR3 = 1./pow((pow(1./ErrCR1,2) + pow(1./ErrCR2,2)),0.5)
+    if not CR1==0:
+        Hist_Sensitivity1.SetBinContent(b+1,5.*pow(ErrCR1*ErrCR1+CR1,0.5)/CR1)
+    if not CR2==0:
+        Hist_Sensitivity2.SetBinContent(b+1,5.*pow(ErrCR2*ErrCR2+CR2,0.5)/CR2)
+    if not CR3==0:
+        Hist_Sensitivity3.SetBinContent(b+1,5.*pow(ErrCR3*ErrCR3+CR3,0.5)/CR3)
+
+canvas = ROOT.TCanvas("canvas","canvas", 200, 10, 600, 600)
+pad1 = ROOT.TPad("pad1","pad1",0,0,1,1)
+pad1.SetBottomMargin(0.15)
+pad1.SetRightMargin(0.15)
+pad1.SetLeftMargin(0.15)
+pad1.SetTopMargin(0.15)
+pad1.SetBorderMode(0)
+pad1.Draw()
+pad1.cd()
+Hist_Sensitivity3.GetYaxis().SetTitle('Excluded signal flux (Bg unit.)')
+Hist_Sensitivity3.GetXaxis().SetTitle(xtitle)
+Hist_Sensitivity3.Draw()
+Hist_Sensitivity2.SetLineColor(2)
+Hist_Sensitivity2.Draw("same")
+Hist_Sensitivity1.SetLineColor(3)
+Hist_Sensitivity1.Draw("same")
+Hist_Sensitivity3.Draw("same")
+if Variable == '_Energy_':
+    pad1.SetLogx()
+canvas.SaveAs('output_plots/Sensitivity_%s.pdf'%(tag))
